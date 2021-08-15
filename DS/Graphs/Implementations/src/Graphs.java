d48d5d9bb1e6873279080f8bc62228f6abc87eed
Mohit Jain x64 <mohit20221@iiitd.ac.in>
2021-08-15 14:50:53 +0530

    graphs folders initialized

---
+++ DS/Graphs/Implementations/src/Graphs.java
@@ -0,0 +1,8 @@
+public class Graphs {
+    //graphs come in multitude of forms
+    //cyclic acyclic,  weighted unweighted, directed undirected  
+    //they can be implemented using a nodelist or a nodematrix
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, Graphs!");
+    }
+}
